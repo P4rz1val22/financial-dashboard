804bbf3acf2bda9ab090160064f6b0eb204c5828
P4rz1val22 <luis.sar.cor@gmail.com>
2025-07-08 16:08:29 -0500

    fixed styling and card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import Card from "@/components/card";
+
 function App() {
   return (
-    <div className="h-screen bg-red-500 w-full">
-      <div className="bg-white rounded-lg shadow-xl">
+    <div className="p-4 bg-gray-200 flex flex-col gap-2 w-screen h-screen">
+      <div className="bg-white rounded-lg shadow-md p-4 transition ">
         <h1 className="text-3xl font-bold text-gray-800">
           Financial Dashboard
         </h1>
-        <p className="text-gray-600">Tailwind + Vite + Bun working! 🚀</p>
+      </div>
+      <div className="bg-white rounded-lg shadow-md p-2 transition">
+        <h1 className="text-xl font-light text-gray-800">
+          <input
+            className="w-full focus-visible:!!!ring-offset-8 p-2"
+            placeholder={"Search for a Stock"}
+          ></input>
+        </h1>
+      </div>
+      <div className="w-full grid-cols-3 gap-2 grid">
+        <Card />
       </div>
     </div>
   );
